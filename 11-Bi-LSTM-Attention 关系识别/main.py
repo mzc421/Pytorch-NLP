fcca4deeb9534f456a873b5bf5910d49b948d0a8
mzc421 <88226175+mzc421@users.noreply.github.com>
2023-09-12 18:30:45 +0800

    11-Bi-LSTM-Attention 关系识别

---
+++ 11-Bi-LSTM-Attention 关系识别/main.py
@@ -0,0 +1,109 @@
+# -*- coding:utf-8 -*-
+# @author: 木子川
+# @Email:  m21z50c71@163.com
+# @VX：fylaicai(回复消息快)
+# @QQ：2463739729
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from model import BiLSTM_ATT
+from config import hparams as hp
+from utils import word2id, max_text, relation2id, NerDataLoader
+from torch.utils.data import DataLoader
+
+
+def train(train_loader, test_loader, model, criterion):
+    f1_max = 0
+    for epoch in range(hp.epochs):
+        flag = False
+        epoch_train_acc, epoch_train_total, epoch_train_loss = 0, 0, 0
+        # 更新进度条的前缀文本
+        for batch_data in train_loader:
+            sentence = batch_data[0].to(hp.device)
+            pos1 = batch_data[1].to(hp.device)
+            pos2 = batch_data[2].to(hp.device)
+            tag = batch_data[3].to(hp.device)
+
+            # [batch_size, tag_size]
+            y = model(sentence, pos1, pos2)
+            loss = criterion(y, tag)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            epoch_train_loss += loss.item()
+            # 计算训练过程中的准确率
+            y = np.argmax(y.cpu().data.numpy(), axis=1)
+
+            for y1, y2 in zip(y, tag):
+                epoch_train_total += 1
+                if y1 == y2:
+                    epoch_train_acc += 1
+
+        # 每一类的预测结果数量
+        count_predict = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # 每一类的真实结果数量
+        count_total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # 每一类的预测正确结果数量
+        count_right = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        with torch.no_grad():
+            for batch_data in test_loader:
+                sentence = batch_data[0].to(hp.device)
+                pos1 = batch_data[1].to(hp.device)
+                pos2 = batch_data[2].to(hp.device)
+                tag = batch_data[3].to(hp.device)
+
+                y = model(sentence, pos1, pos2)
+                y = np.argmax(y.cpu().data.numpy(), axis=1)
+
+                for y1, y2 in zip(y, tag):
+                    count_predict[y1] += 1
+                    count_total[y2] += 1
+                    if y1 == y2:
+                        count_right[y1] += 1
+
+        precision = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        recall = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for i in range(len(count_predict)):
+            if count_predict[i] != 0:
+                precision[i] = float(count_right[i]) / count_predict[i]
+            if count_total[i] != 0:
+                recall[i] = float(count_right[i]) / count_total[i]
+
+        precision = sum(precision) / len(relation2id)
+        recall = sum(recall) / len(relation2id)
+        f1 = (2 * precision * recall) / (precision + recall)
+
+        if f1_max < f1:
+            f1_max = f1
+            torch.save(model.state_dict(), hp.model_best)
+            flag = True
+
+        torch.save(model.state_dict(), hp.model_last)
+        print(f"[{epoch}/{hp.epochs}] train: loss={100 * float(epoch_train_acc) / epoch_train_total: .4f}%;"
+              f"test: acc={100 * precision: .4f}% recall={100 * recall: .4f}% F1={100 * f1: .4f}%"
+              f" {'*' if flag else ''}")
+
+
+if __name__ == "__main__":
+    train_datasets = NerDataLoader("Train")
+    train_loader = DataLoader(train_datasets, hp.batch_size, shuffle=False, num_workers=2,
+                              collate_fn=train_datasets.padding)
+
+    test_datasets = NerDataLoader("Test")
+    test_loader = DataLoader(test_datasets, hp.batch_size, shuffle=False, num_workers=2,
+                             collate_fn=test_datasets.padding)
+
+    # 更新词嵌入的维度
+    hp.embedding_size = len(word2id)
+    hp.pos_size = max_text + 2
+    # model
+    model = BiLSTM_ATT(hp).to(hp.device)
+    optimizer = optim.Adam(model.parameters(), lr=hp.learning_rate, weight_decay=1e-5)
+    criterion = nn.CrossEntropyLoss()
+    train(train_loader, test_loader, model, criterion)
+
+
---
+++ 11-Bi-LSTM-Attention 关系识别/predict.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+# @author: 木子川
+# @Email:  m21z50c71@163.com
+# @VX：fylaicai(回复消息快)
+# @QQ：2463739729
+
+import numpy as np
+import torch
+from model import BiLSTM_ATT
+from config import hparams as hp
+from utils import pos, id2relation
+import pickle
+
+with open(hp.param_dict, 'rb') as f:
+    my_dict = pickle.load(f)
+
+word2id = my_dict["word2id"]
+word2id_len = my_dict["word2id_len"]
+max_text = my_dict["max_text"]
+
+
+def load():
+    hp.embedding_size = word2id_len
+    hp.pos_size = max_text
+    hp.batch_size = 1
+    model = BiLSTM_ATT(hp).to(hp.device)
+    model.load_state_dict(torch.load(hp.model_best))
+    model.eval()
+    return model
+
+
+def process_text(content, ent1, ent2):
+    entity1, entity2 = [], []
+    # 得到实体的位置
+    index1 = content.index(ent1)
+    index2 = content.index(ent2)
+
+    # 以实体为中心，得到实体列表
+    for i in range(len(content)):
+        entity1.append(i - index1)
+        entity2.append(i - index2)
+
+    text = [[word2id.get(word, word2id.get("UNKNOW")) for word in content]]
+    text_position1 = [[pos(num) for num in entity1]]
+    text_position2 = [[pos(num) for num in entity2]]
+
+    return (torch.tensor(text, dtype=torch.long), torch.tensor(text_position1, dtype=torch.long),
+            torch.tensor(text_position2, dtype=torch.long))
+
+
+if __name__ == "__main__":
+    model = load()
+    text = input("请输入文本：")
+    ent1 = input("请输入文本中的实体1：")
+    ent2 = input("请输入文本中的实体2：")
+    loader = process_text(text, ent1, ent2)
+    with torch.no_grad():
+        sentence = loader[0].to(hp.device)
+        pos1 = loader[1].to(hp.device)
+        pos2 = loader[2].to(hp.device)
+
+        y = model(sentence, pos1, pos2)
+        y = np.argmax(y.cpu().data.numpy(), axis=1)[0]
+        print(f"{text}中的{ent1}与{ent2}的关系为：{id2relation[y]}")
+
