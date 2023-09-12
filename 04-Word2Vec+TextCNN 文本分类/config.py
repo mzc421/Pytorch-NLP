1e585c492a29523bcae63b3e8144f0c0241db0c4
mzc421 <88226175+mzc421@users.noreply.github.com>
2023-09-12 18:40:34 +0800

    /04-Word2Vec+TextCNN 文本分类

---
+++ 04-Word2Vec+TextCNN 文本分类/config.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+# @author: 木子川
+# @Email:  m21z50c71@163.com
+# @QQ交流群：830200766
+# @QQ个人：2463739729
+
+
+import argparse
+import os.path
+
+
+def parsers():
+    parser = argparse.ArgumentParser(description="Word2Vec-TextCNN model of argparse")
+    parser.add_argument("--train_file", type=str, default=os.path.join("data", "train.txt"))
+    parser.add_argument("--dev_file", type=str, default=os.path.join("data", "dev.txt"))
+    parser.add_argument("--test_file", type=str, default=os.path.join("data", "test.txt"))
+    parser.add_argument("--classification", type=str, default=os.path.join("data", "class.txt"))
+    parser.add_argument("--data_model", type=str, default=os.path.join("data", "words.bin"))
+    parser.add_argument("--class_num", type=int, default=10)
+    parser.add_argument("--max_len", type=int, default=38)
+    parser.add_argument("--embedding_num", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--learn_rate", type=float, default=1e-3)
+    parser.add_argument("--num_filters", type=int, default=2, help="卷积产生的通道数")
+    parser.add_argument("--save_model_best", type=str, default=os.path.join("model", "best_model.pth"))
+    parser.add_argument("--save_model_last", type=str, default=os.path.join("model", "last_model.pth"))
+    args = parser.parse_args()
+    return args
---
+++ 04-Word2Vec+TextCNN 文本分类/model.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+# @author: 木子川
+# @Email:  m21z50c71@163.com
+# @QQ交流群：830200766
+# @QQ个人：2463739729
+
+import torch.nn as nn
+import torch
+
+
+class Block(nn.Module):
+    def __init__(self, kernel_s, embeddin_num, max_len, hidden_num):
+        super().__init__()
+        # shape [batch *  in_channel * max_len * emb_num]
+        self.cnn = nn.Conv2d(in_channels=1, out_channels=hidden_num, kernel_size=(kernel_s, embeddin_num))
+        self.act = nn.ReLU()
+        self.mxp = nn.MaxPool1d(kernel_size=(max_len - kernel_s + 1))
+
+    def forward(self, batch_emb):  # shape [batch *  in_channel * max_len * emb_num]
+        c = self.cnn(batch_emb)
+        a = self.act(c)
+        a = a.squeeze(dim=-1)
+        m = self.mxp(a)
+        m = m.squeeze(dim=-1)
+        return m
+
+
+class TextCNNModel(nn.Module):
+    def __init__(self, embedding_num, max_len, class_num, hidden_num):
+        super().__init__()
+        self.emb_num = embedding_num
+
+        self.block1 = Block(2, self.emb_num, max_len, hidden_num)
+        self.block2 = Block(3, self.emb_num, max_len, hidden_num)
+        self.block3 = Block(4, self.emb_num, max_len, hidden_num)
+
+        self.classifier = nn.Linear(hidden_num * 3, class_num)  # 2 * 3
+        self.loss_fun = nn.CrossEntropyLoss()
+
+    def forward(self, batch_idx):   # shape torch.Size([batch_size, 1, max_len, embedding])
+        b1_result = self.block1(batch_idx)  # shape torch.Size([batch_size, 2])
+        b2_result = self.block2(batch_idx)  # shape torch.Size([batch_size, 2])
+        b3_result = self.block3(batch_idx)  # shape torch.Size([batch_size, 2])
+
+        # 拼接
+        feature = torch.cat([b1_result, b2_result, b3_result], dim=1)  # shape torch.Size([batch_size, 6])
+        pre = self.classifier(feature)  # shape torch.Size([batch_size, class_num])
+
+        return pre
+
